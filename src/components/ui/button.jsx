5abbeeed580d3b14e258ac3b7174db981a8f9b2d
Shivam Bisht <rajshivam2745@gmail.com>
2024-10-07 01:01:39 +0530

    carousel add and buttons

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -16,6 +16,8 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        blue: "bg-blue-500 hover:bg-blue-600",
+        green:"bg-green-500 hover:bg-green-600",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
@@ -23,6 +25,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
+        xl: "h-14 sm:h-13 rounded-md px-8 text-lg sm:text-l font-semibold",
         icon: "h-10 w-10",
       },
     },
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,15 +1,67 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
+import { Link } from 'react-router-dom'
+import companies from "../data/companies.js"
 
 function LandingPage() {
   return (
-    <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
-      <section className="text-center ">
-        <h1 className="flex flex-col items-center justify-center gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">Find Your Dream Job <span>and get <img src="/logo.png" alt="Hirrd logo" className='h-14 sm:h-24 lg:h-32' /> </span> </h1>
+    <main
+      className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
+
+      <section
+        className="text-center ">
+        <h1
+          className="flex flex-col items-center justify-center gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">Find Your Dream Job
+          <span className='flex items-center gap-3 gradient-title'>and get <img src="/logo.png" alt="Hirrd logo" className='h-14 sm:h-24 lg:h-32' />
+          </span>
+        </h1>
+
+        <p
+          className="text-gray-300 sm:mt-5 text-xs sm:text-xl">
+          Explore thousands of job listings or find the perfect candidate
+        </p>
+
       </section>
-      <div>
-        {/* buttons */}
-        {/* carousel */}
+
+      <div className='flex justify-center items-center gap-2 -top-2'>
+        <Link to="/jobs">
+          <Button
+            variant="blue"
+            size="xl">
+            Find Jobs
+          </Button>
+        </Link>
+
+        <Link to="/post-job">
+          <Button
+            variant="green"
+            size="xl">
+            Post A Job
+          </Button>
+
+        </Link>
+
       </div>
+
+      <Carousel
+        plugins={[    
+        ]}
+        className="w-full py-10"
+      >
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => (
+            <CarouselItem key={id} className="basis-1/3 lg:basis-1/6 ">
+              <img
+                src={path}
+                alt={name}
+                className="h-9 sm:h-14 object-contain"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+
       {/* banner */}
       <section>
         {/* cards */}
